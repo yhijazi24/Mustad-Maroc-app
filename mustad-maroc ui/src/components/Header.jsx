09b50449e68c90f75e4932fbc4ac3adcd5a26820
yhijazi24 <yahyahijazi643@gmail.com>
2024-08-24 23:48:11 +0100

    commit

--- mustad-maroc ui/src/components/Header.jsx
+++ mustad-maroc ui/src/components/Header.jsx
@@ -7,13 +7,17 @@ const Header = () => {
     <div className='header-container'>
       {headerItems.map(item => (
         <div key={item.id} className='header-wrapper'>
-          <img src={item.img} className='header-img' alt='background' />
-          <div className='header-content'>
-            <h1 className='header-title'>{item.title}</h1>
-            <p className='header-desc'>{item.desc}</p>
+          <div className='header-img-container'>
+            <img src={item.img} className='header-img' alt='background' />
           </div>
-          <div className='header-button-wrapper'>
-            <button className='header-button'>Get In Touch</button>
+          <div className='header-info'>
+            <div className='header-content'>
+              <h1 className='header-title'>{item.title}</h1>
+              <p className='header-desc'>{item.desc}</p>
+            </div>
+            <div className='header-button-wrapper'>
+              <button className='header-button'>GET IN TOUCH</button>
+            </div>
           </div>
         </div>
       ))}
